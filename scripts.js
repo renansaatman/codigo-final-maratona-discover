06e80a6e694c8499bda6e2d277689ad46aa07d4e
Renan Saatman <renansaatman@hotmail.com>
2021-02-04 19:12:32 -0300

    Fixing bug - formatAmount function

--- scripts.js
+++ scripts.js
@@ -87,8 +87,8 @@ const DOM = {
 
 const Utils = {
     formatAmount(value){
-        value = Number(value) * 100
-        return value
+        value = value * 100
+        return Math.round(value)
     },
     formatDate(date){
         const splittedDate = date.split("-")
